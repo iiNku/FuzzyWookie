00c3a4dfac2eaa04743461b254e7c8ef10ff4cbc
Nathou <Nathou@Nathou-PC>
2015-04-30 15:31:16 +0200

    add

---
+++ src/fr/polyteck/fuzzywookie/utils/Parser.java
@@ -0,0 +1,59 @@
+package fr.polyteck.fuzzywookie.utils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import fr.polytech.fuzzywookie.metier.Image;
+import fr.polytech.fuzzywookie.metier.Print;
+
+public class Parser {
+	
+	public static void parserFile(String file, Print print)
+	{
+		String chaine="";
+		InputStream ips;
+		try {
+			ips = new FileInputStream(file);
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			int counter = 0;
+			while ((ligne=br.readLine())!=null){
+				if(counter == 0)
+				{
+					print.setPatternX(Integer.valueOf(ligne.substring(ligne.indexOf("=")+1, ligne.length())));
+				}
+				else if(counter == 1)
+				{
+					print.setPatternY(Integer.valueOf(ligne.substring(ligne.indexOf("=")+1, ligne.length())));
+				}
+				else if(counter == 2)
+				{
+					print.setPricePattern(Integer.valueOf(ligne.substring(ligne.indexOf("=")+1, ligne.length())));
+				}
+				else
+				{
+					String[] imageValue = ligne.split(" ");
+					Image img = new Image(Integer.valueOf(imageValue[0]), Integer.valueOf(imageValue[1]), "Image"+(counter-2));
+					img.setNbItem(Integer.valueOf(imageValue[2]));
+				}
+				System.out.println(ligne);
+				chaine+=ligne+"\n";
+				counter++;
+			}
+			br.close(); 
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+	}
+
+}
--- src/fr/polyteck/fuzzywookie/utils/Utils.java
+++
@@ -1,5 +0,0 @@
-package fr.polyteck.fuzzywookie.utils;
-
-public class Utils {
-
-}
