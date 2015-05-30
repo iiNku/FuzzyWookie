31d3aeafd0db4071ee9466452674fdf252524c57
Jocelyn <jocelyn.poncelet@gmail.com>
2015-05-30 14:58:22 +0200

    no message

--- src/fr/polytech/fuzzywookie/reproduction/Reproduction.java
+++ src/fr/polytech/fuzzywookie/reproduction/Reproduction.java
@@ -7,9 +7,9 @@ import fr.polytech.fuzzywookie.metier.*;
 import fr.polytech.fuzzywookie.pack.PackingTest;
 
 public class Reproduction {
-    private ArrayList<Pattern> fils = new ArrayList<Pattern>();
     
-    public ArrayList<Pattern> ReproductionPattern(Print p1,Print p2){
+    public Print ReproductionPattern(Print p1,Print p2){
+    	Print fils = new Print(p1.getProject());
         List<Pattern> patterns1 = p1.getListPattern();
         List<Pattern> patterns2 = p2.getListPattern();
         int maxPattern1 = patterns1.size();
@@ -22,32 +22,31 @@ public class Reproduction {
         int getpat1;
         int getpat2;
         if(typeRepro ==  1){
-            while(fils.size()<meanPattern/2 && (i+1)<maxPattern1){
-                fils.add(patterns1.get(i));
+            while(fils.getListPattern().size()<meanPattern/2 && (i+1)<maxPattern1){
+                fils.getListPattern().add(patterns1.get(i));
                 i++;
             }
-            while(fils.size()<meanPattern && (i+1)<maxPattern2){
-                fils.add(patterns2.get(i));
+            while(fils.getListPattern().size()<meanPattern && (i+1)<maxPattern2){
+                fils.getListPattern().add(patterns2.get(i));
                 i++;
             }
         }else{
             int change =(int) Math.random()*minPattern;
-            while(fils.size()<meanPattern){
+            while(fils.getListPattern().size()<meanPattern){
                 getpat1 = 0;
                 while(getpat1<change && patterns1.get(i)!=null){
-                    fils.add(patterns1.get(i));
+                    fils.getListPattern().add(patterns1.get(i));
                     i++;
                     getpat1++;
                 }
                 getpat2 = 0;
                 while(getpat2<change && patterns2.get(i)!=null){
-                    fils.add(patterns2.get(i));
+                    fils.getListPattern().add(patterns2.get(i));
                     i++;
                     getpat2++;
                 }
             }
         }
-        
         // test fils
         return fils;
     }
